4170e91d0e964f2f5205ac836f3b0574da78921a
Ahmed Ali IBRAHIM <ahmedaliibrahim01@gmail.com>
2024-02-23 22:33:20 +0300

    Created PrimeNumber file

--- src/Java_36_PrimeNumbers.java
+++ src/Java_36_PrimeNumbers.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class Java_36_PrimeNumbers {
+    public static void main(String[] args) {
+        for(int i=2; i<=100; i++){
+            boolean isPrime= true;
+            for(int j=2; j<i;j++){
+                if(i%j==0){
+                    isPrime= false;
+                    break;
+                }
+            }
+            if(isPrime){
+                System.out.print(i +" ");
+            }
+        }
+    }
 }
