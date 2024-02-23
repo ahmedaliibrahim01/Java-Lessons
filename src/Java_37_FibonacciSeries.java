e756cea6e18822d8d79209604f17ec3ade98b25a
Ahmed Ali IBRAHIM <ahmedaliibrahim01@gmail.com>
2024-02-24 01:10:09 +0300

    Created Fibonacci Series File

--- src/Java_37_FibonacciSeries.java
+++ src/Java_37_FibonacciSeries.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class Java_37_FibonacciSeries {
+    public static void main(String[] args) {
+        // 0 1 1 2 3 5 8 13 21 ......
+        Scanner input = new Scanner(System.in);
+        int number, total, firstNumber = 0, secondNumber = 1;
+        number = input.nextInt();
+
+        System.out.print(number + " Element Fibonacci Series : ");
+
+        for (int i = 1; i <= number; i++) {
+            System.out.print(firstNumber + " ");
+            total = firstNumber + secondNumber;
+            firstNumber = secondNumber;
+            secondNumber = total;
+        }
+    }
 }
