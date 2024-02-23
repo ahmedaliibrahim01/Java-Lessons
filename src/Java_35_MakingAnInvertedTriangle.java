54bbecba2cd224c41186f81b6824d05f8c6588b3
Ahmed Ali IBRAHIM <ahmedaliibrahim01@gmail.com>
2024-02-23 23:38:57 +0300

    Created Making An Inverted Triangle file

--- src/Java_35_MakingAnInvertedTriangle.java
+++ src/Java_35_MakingAnInvertedTriangle.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class Java_35_MakingAnInvertedTriangle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= (n-i); j++) {
+                System.out.print("*");
+            }
+            System.out.println("*");
+
+        }
+    }
+
 }
