232ac27b23506291a43c149080cb996bdae2f79f
Ahmed Ali IBRAHIM <ahmedaliibrahim01@gmail.com>
2024-02-24 23:07:41 +0300

    Making A Triangle WithStars

--- src/Java_28_MakingATriangleWithStars_1.java
+++ src/Java_28_MakingATriangleWithStars_1.java
@@ -1,2 +1,29 @@
+import java.util.Scanner;
+
 public class Java_28_MakingATriangleWithStars_1 {
+    public static void main(String[] args) {
+        Scanner klavye = new Scanner(System.in);
+        System.out.print("Bir Sayı Giriniz : ");
+        int n = klavye.nextInt();
+
+        for (int i = 0; i <= n ; i++) {
+            for (int j = 0; j < (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
+        for (int i = n; i >= 1 ; i--) {
+            for (int j = 1; j <= (n - i + 1); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
 }
