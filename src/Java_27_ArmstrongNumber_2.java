a5db8c665cb23b5713feaac22155773666423101
Ahmed Ali IBRAHIM <ahmedaliibrahim01@gmail.com>
2024-02-24 04:27:13 +0300

    Created Armstrong Number file

--- src/Java_27_ArmstrongNumber_2.java
+++ src/Java_27_ArmstrongNumber_2.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
 public class Java_27_ArmstrongNumber_2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number, basNumber, total = 0;
+        System.out.print("Enter Number : ");
+        number = input.nextInt();
+
+        while (number != 0){
+            basNumber = number % 10;
+            total = total + basNumber;
+            number = number / 10;
+        }
+        System.out.println("Sum of digits of the number : " + total);
+    }
 }
