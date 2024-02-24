b8c25d672255055f230218eed08f1059f1bf8e9b
Ahmed Ali IBRAHIM <ahmedaliibrahim01@gmail.com>
2024-02-24 12:02:27 +0300

    Updated Harmonic Number file

--- src/Java_28_HarmonicNumbers_1.java
+++ src/Java_28_HarmonicNumbers_1.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Java_28_HarmonicNumbers_1 {
     public static void main(String[] args) {
-        double enterNumber, harmonicNumber = 0, number = 1, total = 0, result = 0;
+        double enterNumber, result = 0;
         Scanner input = new Scanner(System.in);
         System.out.print("Enter Number : ");
         enterNumber = input.nextInt();
